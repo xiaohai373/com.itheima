56d3c50f02c418cf33f1a6410c09285efcb4fe19
tom <tom@itheima.com>
2018-11-05 19:29:23 +0800

    将java工程上传到git

---
+++ src/main/java/com/itheima/TestGit.java
@@ -0,0 +1,9 @@
+package com.itheima;
+
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+    }
+}
